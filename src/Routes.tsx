c2ac5b5d7b38c2eda49dcf0d013c8b835224fdd0
RA4Z <rraz639@gmail.com>
2024-03-22 16:39:00 -0300

    Adicionando agenda da Marguit

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Automatization from './pages/Automatization';
+import Agenda from 'pages/Agenda';
 
 export default function AppRouter() {
   return (
@@ -8,6 +9,7 @@ export default function AppRouter() {
       <Router>
         <Routes>
           <Route path='/' element={<Homepage />} />
+          <Route path='/Agenda' element={<Agenda />} />
           <Route path='/Automatization/:id' element={<Automatization />} />
         </Routes>
       </Router>
---
+++ src/pages/Agenda/index.tsx
@@ -0,0 +1,49 @@
+import Header from 'components/Header'
+import styles from './Agenda.module.scss'
+import { Divider, TextField } from '@mui/material'
+import Footer from 'components/Footer'
+import DataJSON from 'data/Agenda.json'
+import { useEffect, useState } from 'react'
+
+export default function Agenda() {
+    const [data, setData] = useState(DataJSON)
+    const [filter, setFilter] = useState('')
+
+    useEffect(() => {
+        function findText(text: string) {
+            const regex = new RegExp(filter, 'i');
+            return regex.test(text);
+        }
+        let newList = DataJSON.filter(item => findText(item.DESCRIÇÃO) || findText(item.DETALHES!) || findText(item.REFERÊNCIA) || findText(item.UTILIDADE) || findText(item.SEQ.toString()))
+        setData(newList)
+    }, [filter])
+    return (
+        <>
+            <Header />
+            <div className={styles.header}>
+                <TextField
+                    value={filter}
+                    onChange={e => setFilter(e.target.value)}
+                    label="Filtrar por Palavra-Chave"
+                    variant="filled"
+                    style={{ backgroundColor: 'white' }}
+                />
+            </div>
+            <Divider style={{ background: 'white' }} />
+            <div className={styles.container}>
+                <div className={styles.projects}>
+                    {data.map((data, index) => (
+                        <div key={index} className={styles.card}>
+                            <h3>Referência:</h3><p>{data.REFERÊNCIA}</p>
+                            <h3>Detalhes:</h3><p>{data.DETALHES}</p>
+                            <h3>Descrição:</h3><p>{data.DESCRIÇÃO}</p>
+                            <h3>Utilidade:</h3><p>{data.UTILIDADE}</p>
+                            <Divider style={{ background: 'white', width: '80%' }} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
