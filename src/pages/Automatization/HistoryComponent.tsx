af6f7e76fe1e8448ffd841c219b89b63bc243b07
RA4Z <rraz639@gmail.com>
2024-03-22 14:27:01 -0300

    Verificando se a última execução foi a mais de 7 dias

--- src/pages/Automatization/HistoryComponent.tsx
+++ src/pages/Automatization/HistoryComponent.tsx
@@ -10,17 +10,29 @@ interface Props {
 
 const HistoryComponent: React.FC<Props> = ({ automationName, history }) => {
     const [data, setData] = useState<HistoryItemType[]>([])
+    const [sevenDays, setSevenDays] = useState(false)
 
     useEffect(() => {
         async function getFirebaseData() {
             if (data.length === 0) await getHistory(`Algoritmos/${automationName}/${history.id}`, setData)
+            if (data.length > 0) {
+                const dataAtual = new Date();
+                const seteDiasEmMilissegundos = 7 * 24 * 60 * 60 * 1000;
+                const dataDoItem = new Date(data[0].horaExec);
+                const diferencaDeTempoEmMilissegundos = dataAtual.getTime() - dataDoItem.getTime();
+                if (diferencaDeTempoEmMilissegundos > seteDiasEmMilissegundos) {
+                    setSevenDays(true)
+                } else {
+                    setSevenDays(false)
+                }
+            }
         }
         getFirebaseData()
     }, [automationName, history.id, data])
 
     return (
         <div>
-            <h4>{history.id}</h4>
+            <h4 style={sevenDays ? { color: 'black' } : { color: 'white' }}>{history.id}</h4>
             <ul className={styles.history__stack}>
                 {data.map(item => (
                     <li key={item.id} className={styles.history__list}>
