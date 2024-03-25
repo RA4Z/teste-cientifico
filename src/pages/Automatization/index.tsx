f5af5d0e1b68fd5aeb75ab3adf15dc7724b61b45
RA4Z <rraz639@gmail.com>
2024-03-25 13:23:00 -0300

    Adicionando header na tela de ver mais

--- src/pages/Automatization/index.tsx
+++ src/pages/Automatization/index.tsx
@@ -11,6 +11,7 @@ import { infoAutomation } from 'services/firestore'
 import { getHistory } from 'services/database'
 import HistoryComponent from './HistoryComponent'
 import Footer from 'components/Footer'
+import Header from 'components/Header'
 
 export default function Automatization() {
     const [automation, setAutomation] = useState<AutomationType>()
@@ -29,26 +30,29 @@ export default function Automatization() {
         getFirebaseData()
     }, [id, automation?.nome])
     return (
-        <div className={styles.container}>
-            <div className={styles.container__title}>
-                <img src={VoltarIMG} alt='Voltar' onClick={() => navigate(-1)} />
-                <h2>{automation?.nome}</h2>
-            </div>
-            <div className={styles.container__text}>
-                <li className={styles.container__text__description}>{automation?.descricao}</li>
-                <li>Automatização desenvolvida em {automation?.data_desenvolvimento} por {automation?.desenvolvedor} e solicitado por {automation?.solicitante}</li>
-                <li className={styles.container__text__path}>Automatização se encontra em {automation?.caminho_groups}</li>
-            </div>
-            <img src={automation?.imagem} alt='Ícone do projeto' className={styles.container__icon} />
-            <Divider style={{ background: 'white' }} />
+        <>
+            <Header />
+            <div className={styles.container}>
+                <div className={styles.container__title}>
+                    <img src={VoltarIMG} alt='Voltar' onClick={() => navigate(-1)} />
+                    <h2>{automation?.nome}</h2>
+                </div>
+                <div className={styles.container__text}>
+                    <li className={styles.container__text__description}>{automation?.descricao}</li>
+                    <li>Automatização desenvolvida em {automation?.data_desenvolvimento} por {automation?.desenvolvedor} e solicitado por {automation?.solicitante}</li>
+                    <li className={styles.container__text__path}>Automatização se encontra em {automation?.caminho_groups}</li>
+                </div>
+                <img src={automation?.imagem} alt='Ícone do projeto' className={styles.container__icon} />
+                <Divider style={{ background: 'white' }} />
 
-            <h3 className={styles.history}>Histórico de execuções...</h3>
-            <div className={styles.history}>
-                {(history.length > 0 && history !== undefined && automation?.nome !== undefined) && history.map((logged, index) => (
-                    <HistoryComponent key={index} automationName={automation.nome} history={logged} />
-                ))}
+                <h3 className={styles.history}>Histórico de execuções...</h3>
+                <div className={styles.history}>
+                    {(history.length > 0 && history !== undefined && automation?.nome !== undefined) && history.map((logged, index) => (
+                        <HistoryComponent key={index} automationName={automation.nome} history={logged} />
+                    ))}
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </>
     )
 }
