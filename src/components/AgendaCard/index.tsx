04c7fb763303b4aaf7e1033c825ad99db392ce34
RA4Z <rraz639@gmail.com>
2024-03-25 08:14:15 -0300

    Criando componente para card da agenda

---
+++ src/components/AgendaCard/index.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ideiaIMG from 'assets/ideia.png'
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+interface Props {
+    referencia: string,
+    detalhes: string,
+    descricao: string,
+    utilidade: string
+}
+
+export default function AgendaCard(props: Props) {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Card sx={{ width: 345 }}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe">
+                        <img src={ideiaIMG} style={{height:'100%'}} alt='imagem' />
+                    </Avatar>
+                }
+                title={`${props.referencia}`}
+                subheader={`${props.utilidade}`}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {props.descricao}
+                </Typography>
+            </CardContent>
+            {props.detalhes !== '' && <>
+                <CardActions disableSpacing>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>
+                            {props.detalhes}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </>}
+        </Card>
+    );
+}
--- src/pages/Agenda/index.tsx
+++ src/pages/Agenda/index.tsx
@@ -4,6 +4,7 @@ import { Divider, TextField } from '@mui/material'
 import Footer from 'components/Footer'
 import DataJSON from 'data/Agenda.json'
 import { useEffect, useState } from 'react'
+import AgendaCard from 'components/AgendaCard'
 
 export default function Agenda() {
     const [data, setData] = useState(DataJSON)
@@ -33,13 +34,17 @@ export default function Agenda() {
             <div className={styles.container}>
                 <div className={styles.projects}>
                     {data.map((data, index) => (
-                        <div key={index} className={styles.card}>
-                            <h3>Referência:</h3><p>{data.REFERÊNCIA}</p>
-                            <h3>Detalhes:</h3><p>{data.DETALHES}</p>
-                            <h3>Descrição:</h3><p>{data.DESCRIÇÃO}</p>
-                            <h3>Utilidade:</h3><p>{data.UTILIDADE}</p>
-                            <Divider style={{ background: 'white', width: '80%' }} />
-                        </div>
+                        // <div key={index} className={styles.card}>
+                        //     <h3>Referência:</h3><p>{data.REFERÊNCIA}</p>
+                        //     <h3>Detalhes:</h3><p>{data.DETALHES}</p>
+                        //     <h3>Descrição:</h3><p>{data.DESCRIÇÃO}</p>
+                        //     <h3>Utilidade:</h3><p>{data.UTILIDADE}</p>
+                        //     <Divider style={{ background: 'white', width: '80%' }} />
+                        // </div>
+                        <AgendaCard referencia={data.REFERÊNCIA}
+                            descricao={data.DESCRIÇÃO}
+                            detalhes={data.DETALHES ? data.DETALHES : ''}
+                            utilidade={data.UTILIDADE} />
                     ))}
                 </div>
             </div>
