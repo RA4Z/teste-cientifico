d81d224e8653d7184bc9c370e129c5b70178cd72
RA4Z <rraz639@gmail.com>
2024-03-25 13:34:21 -0300

    Adicionando suspense no projeto

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Automatization from './pages/Automatization';
-import Agenda from 'pages/Agenda';
+import { lazy, Suspense } from 'react'
+import { Box, LinearProgress } from '@mui/material';
+
+const Homepage = lazy(() => import('pages/Homepage'));
+const Automatization = lazy(() => import('pages/Automatization'));
+const Agenda = lazy(() => import('pages/Agenda'));
 
 export default function AppRouter() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/Agenda' element={<Agenda />} />
-          <Route path='/Automatization/:id' element={<Automatization />} />
-        </Routes>
+        <Suspense fallback={<Box sx={{ width: '100%' }}>
+          <LinearProgress />
+        </Box>}>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/Agenda' element={<Agenda />} />
+            <Route path='/Automatization/:id' element={<Automatization />} />
+          </Routes>
+        </Suspense>
       </Router>
     </>
   );
