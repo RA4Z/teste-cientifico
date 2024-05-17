1fec29b2c7de18f7e75cd38ccac43d88adbde211
RA4Z <rraz639@gmail.com>
2024-05-17 09:43:43 -0300

    Adicionando efeito de loading

--- src/components/ChatBot/index.tsx
+++ src/components/ChatBot/index.tsx
@@ -4,6 +4,7 @@ import Cancelar from '@mui/icons-material/Close';
 import { Dialog, TextField } from '@mui/material';
 import { useState } from "react";
 import { runChat } from './chat'
+import Loading from 'components/Loading';
 
 interface Props {
     visible: any,
@@ -18,9 +19,11 @@ interface historico {
 
 export default function ChatBot({ visible, setVisible }: Props) {
     const [history, setHistory] = useState<historico[]>([])
+    const [loading, setLoading] = useState(false)
     const [question, setQuestion] = useState('')
 
     async function sendMessage() {
+        setLoading(true)
         const updatedHistory = [...history]
         updatedHistory.push({ text: question, type: 'question' })
         setHistory(updatedHistory)
@@ -45,54 +48,54 @@ export default function ChatBot({ visible, setVisible }: Props) {
         const updatedAnswer = [...updatedHistory];
         updatedAnswer.push({ text: response, type: 'answer' });
         setHistory(updatedAnswer);
+        setLoading(false)
     }
     return (
         <>
-            <Dialog open={true} style={{ zIndex: '100' }} onClose={() => setVisible(false)}>
-                <div className={styles.container}>
-                    {visible && <div className={styles.container}>
-                        <div className={styles.container__header}>
-                            <li>ChatBot PCP</li>
-                            <Cancelar fontSize='large' onClick={() => setVisible(false)} className={styles.container__header__close} />
-                        </div>
-                        <div id='texto_chat' className={styles.container__chat}>
-                            {history.map((message, index) => (
-                                <div key={index} className={message.type === 'question' ? styles.enviada : styles.recebida}>
-                                    <div dangerouslySetInnerHTML={{ __html: message.text }} />
-                                </div>
-                            ))}
-                        </div>
-                        <div className={styles.sendMessage}>
-                            <TextField
-                                id="text-chat"
-                                label="Escrever Comando"
-                                type='text'
-                                value={question}
-                                onChange={(e) => setQuestion(e.target.value)}
-                                multiline
-                                maxRows={4}
-                                className={styles.input}
-                                onKeyDown={(e) => {
-                                    if (e.key === 'Enter' && !e.shiftKey) {
-                                        e.preventDefault();
-                                        sendMessage();
-                                    }
-                                }}
-                            />
+            <div className={styles.container}>
+                <Loading open={loading} />
+                {visible && <div className={styles.container}>
+                    <div className={styles.container__header}>
+                        <li>ChatBot PCP</li>
+                        <Cancelar fontSize='large' onClick={() => setVisible(false)} className={styles.container__header__close} />
+                    </div>
+                    <div id='texto_chat' className={styles.container__chat}>
+                        {history.map((message, index) => (
+                            <div key={index} className={message.type === 'question' ? styles.enviada : styles.recebida}>
+                                <div dangerouslySetInnerHTML={{ __html: message.text }} />
+                            </div>
+                        ))}
+                    </div>
+                    <div className={styles.sendMessage}>
+                        <TextField
+                            id="text-chat"
+                            label="Escrever Comando"
+                            type='text'
+                            value={question}
+                            onChange={(e) => setQuestion(e.target.value)}
+                            multiline
+                            maxRows={4}
+                            className={styles.input}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter' && !e.shiftKey) {
+                                    e.preventDefault();
+                                    sendMessage();
+                                }
+                            }}
+                        />
 
-                            <Send
-                                fontSize='large'
-                                className={styles.send}
-                                onClick={() => {
-                                    if (question.trim() !== '') {
-                                        sendMessage();
-                                    }
-                                }}
-                            />
-                        </div>
-                    </div>}
-                </div>
-            </Dialog>
+                        <Send
+                            fontSize='large'
+                            className={styles.send}
+                            onClick={() => {
+                                if (question.trim() !== '') {
+                                    sendMessage();
+                                }
+                            }}
+                        />
+                    </div>
+                </div>}
+            </div>
         </>
     )
 }
