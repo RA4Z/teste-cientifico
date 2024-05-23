bee611d9cf78cded97f03ecb7c8286515cabe6c5
RA4Z <rraz639@gmail.com>
2024-05-23 16:22:50 -0300

    Corrigindo problemas de dependências

--- src/components/DefaultPage/index.tsx
+++ src/components/DefaultPage/index.tsx
@@ -1,9 +1,5 @@
 import { Outlet } from 'react-router-dom'
 import styles from './DefaultPage.module.scss'
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 export default function DefaultPage() {
     return (
         <div className={styles.container}>
