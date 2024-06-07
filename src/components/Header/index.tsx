3d6dc5a04909a8854f9b759d1b136753844b4521
RA4Z <rraz639@gmail.com>
2024-06-07 13:39:50 -0300

    Tentativa de redirecionamento para outra página

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,6 +13,7 @@ export default function Header() {
         <div className={styles.container}>
             <button onClick={() => navigateHome()} title='Navegar para a Homepage'>
                 <img src={Weg} alt='Logo da Weg' />
+                <a href='http://10.1.43.24:3000'>Useless</a>
             </button>
         </div>
     )
